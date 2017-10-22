ff92b61584498238a43a7366c1c3f4f7ba23fb07
Quinn <qzapata@uw.edu>
2017-10-22 09:10:37 -0400

    fix

--- calendarsite/catalog/views.py
+++ calendarsite/catalog/views.py
@@ -5,4 +5,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-	return render(request, 'views.html')
+	return render(request, 'index.html')
+
+def task(request):
+	return render(request, 'task.html')
