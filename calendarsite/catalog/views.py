38b378177e744f0a2e33f1c6616bee2c3c3e6bdd
Quinn <qzapata@uw.edu>
2017-10-22 12:53:21 -0400

    update 4 ben

--- calendarsite/catalog/views.py
+++ calendarsite/catalog/views.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.shortcuts import render
 from django.http import HttpResponse
+
+import parser
+
 
 # Create your views here.
 def index(request):
@@ -13,5 +15,6 @@ def task(request):
 
 def get_data(request):
 	data = request.POST.getlist('data[]')
-	print(tasks)
+	print(data)
+
 	return HttpResponse('success')
