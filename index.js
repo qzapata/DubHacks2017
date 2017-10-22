65b50d27ab7eeed99175834e0390f7d86f167266
Quinn <qzapata@uw.edu>
2017-10-21 22:53:59 -0400

    add starter files

---
+++ index.js
@@ -0,0 +1,13 @@
+(function () {
+	//returns the dom element for the given id
+	//
+	//id: string id of an existing dom element
+	function $(id) {
+		return document.getElementById(id);
+	}
+
+	//sets onload function of the page
+	window.onload = function() {
+		
+	}; 
+}) ();
