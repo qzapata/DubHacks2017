78f9fb2583b0f916b88c94df1323523b88d252d3
Ben Trowbridge <bentrow@cs.washington.edu>
2017-10-22 00:41:43 -0700

    finished rudimentary algorithm? untested

--- backend/CalendarMaker.py
+++ backend/CalendarMaker.py
@@ -14,6 +14,7 @@ class CalendarMaker:
     def __init__(self, homeworks, days):
         self.homeworks = homeworks
         self.days = days
+        self.events = []
         for h in homeworks:
             duedate = datetime.date(h.dueDate.year, h.dueDate.month, h.dueDate.day)
             dates = []
@@ -23,7 +24,7 @@ class CalendarMaker:
                 if dateidx == duedate:
                     break
             cal = CalendarSpace(dates)
-
+            self.events.append(self.getEvents(cal, h))
 
     def getPriority(self, homework, time):
         return math.fabs((time.hour * 60 + time.minute) - self.IDEAL_HOUR) + (homework.dueDate.day - time.day)*10
@@ -31,5 +32,14 @@ class CalendarMaker:
     def getEvents(self, calsp, homework):
         segs = homework.intoChunks()
         q = PriorityQueue()
+        events = []
         for e in calsp.times:
-            q.put_nowait(e)
+            q.put_nowait((self.getPriority(homework, e[0]), e))
+        for x in range(0, len(segs)):
+            e = q.get_nowait()[1]
+            while e[0] >= homework.dueDate:
+                e = q.get_nowait()[1]
+            t = datetime.timedelta(0, 0, 0, 0, e[1])
+            events.append(Event(e[0], e[0] + t, homework.name))
+        return events
+
